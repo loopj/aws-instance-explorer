2b69a5b289d7508253a73e1b03a68e6d0b56070f
James Smith <james@loopj.com>
2016-05-29 23:39:50 -0700

    Working grouping

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ class AwsInstanceChart {
       },
 
       // Instance grouping rules
-      groupBy: [],
+      groupBy: null,
 
       // Instance coloring rules
       colorBy: null
@@ -25,14 +25,13 @@ class AwsInstanceChart {
 
     // Instance node data
     this.instances = [];
-    this.rootNode = {name: "root", children: true};
 
     // Configure AWS SDK
     AWS.config.region = 'us-east-1';
 
     // Create a D3 force-directed graph layout
     this.force = d3.layout.force()
-      .charge(-200)
+      .charge(-150)
       .on("tick", () => this.tick());
 
     // Create the SVG drawing canvas
@@ -51,6 +50,7 @@ class AwsInstanceChart {
    * @param el the parent element to populate the chart into
    */
   resize(el) {
+    // Save new size
     this.width = el.offsetWidth;
     this.height = el.offsetHeight;
 
@@ -110,16 +110,16 @@ class AwsInstanceChart {
       "2xlarge", "4xlarge", "8xlarge", "16xlarge", "32xlarge"
     ];
 
-    return d.children ? 3 : Math.pow(instanceTypes.indexOf(d.type.split(".")[1]), 1.4);
+    return d.children ? 3 : Math.pow(instanceTypes.indexOf(d.instanceType.split(".")[1]), 1.4);
   }
 
   /**
    * Find a key in a (potentially) nested object
    * @example
    * // returns "db"
-   * findKey({tags: {Role: "db"}, type: "r3.large"}, "tags.Role")
+   * findKey({tags: {Role: "db"}, instanceType: "r3.large"}, "tags.Role")
    * // returns "r3.large"
-   * findKey({tags: {Role: "db"}, type: "r3.large"}, "type")
+   * findKey({tags: {Role: "db"}, instanceType: "r3.large"}, "instanceType")
    */
   findKey(key, obj) {
     try {
@@ -180,30 +180,28 @@ class AwsInstanceChart {
     return true;
   }
 
+  /**
+   * Draw nodes after updating the data-set or filtering
+   */
   drawNodes(nodes) {
     var nodeSelection = this.svg.selectAll(".node").data(nodes, d => d.id);
 
-    // Create svg group element for each instance
-    var nodeGroup = nodeSelection
-      .enter().append("g")
-        .attr("class", "node")
-        .call(this.force.drag);
-
     // Create circle for each AWS instance - add to svg group
-    nodeGroup.append("circle")
-      .attr("r", d => this.getNodeRadius(d))
-      .style("fill", d => this.getNodeFill(d))
-      .style("stroke", d => this.getNodeStroke(d));
-
-    // Create label for each AWS instance - add to svg group
-    nodeGroup.append("text")
-      .attr("dy", ".35em")
-      .text(d => d.tags ? d.tags.Name : d.name);
+    nodeSelection
+      .enter().append("circle")
+        .attr("class", "node")
+        .attr("r", d => this.getNodeRadius(d))
+        .style("fill", d => this.getNodeFill(d))
+        .style("stroke", d => this.getNodeStroke(d))
+        .call(this.force.drag);
 
     // Remove orphaned node DOM elements
     nodeSelection.exit().remove();
   }
 
+  /**
+   * Draw node links after updating the data-set or filtering
+   */
   drawLinks(links) {
     var linkSelection = this.svg.selectAll(".link").data(links, (d) => {
       return `${d.source.id}-${d.target.id}`;
@@ -222,13 +220,53 @@ class AwsInstanceChart {
    * Update the node data used to generate the chart
    */
   update() {
-    // TODO: Build grouping hierachy here
-    // - build nodes from filtered instance list, with children objects
-    // - use d3.layout.tree to get tree.links(nodes)
-    var nodes = this.instances.filter(d => this.matchesAllFilters(d));
-    nodes.push(this.rootNode);
+    var instances = this.instances.filter(d => this.matchesAllFilters(d));
+    var nodes = [];
 
-    var links = nodes.map(d => ({source: this.rootNode, target: d}));
+    // Construct the root node
+    var root = {
+      id: "root",
+      nodeType: "root",
+      children: []
+    };
+    nodes.push(root);
+
+    // Check if we should group nodes together
+    if(this.settings.groupBy) {
+      // Get a unique set of values for this group key
+      var groupKeys = new Set();
+      instances.forEach(instance => {
+        groupKeys.add(this.findKey(this.settings.groupBy, instance));
+      });
+
+      // Construct each group node
+      groupKeys.forEach(k => {
+        var group = {
+          id: k,
+          nodeType: "group",
+          children: []
+        };
+        nodes.push(group);
+        root.children.push(group);
+
+        // Add all matching instances to this group node
+        instances.forEach(instance => {
+          if(this.findKey(this.settings.groupBy, instance) == k) {
+            nodes.push(instance);
+            group.children.push(instance);
+          }
+        });
+      });
+    } else {
+      // No grouping - just add all instances to root node
+      instances.forEach(instance => {
+        nodes.push(instance);
+        root.children.push(instance);
+      });
+    }
+
+    // Create links from the node tree
+    var links = d3.layout.tree().links(nodes);
 
     // Draw nodes and links
     this.drawNodes(nodes);
@@ -245,8 +283,10 @@ class AwsInstanceChart {
    * Update node positions
    */
   tick() {
-    this.rootNode.x = this.width / 2;
-    this.rootNode.y = this.height / 2;
+    // Reposition nodes
+    this.svg.selectAll(".node")
+      .attr("cx", d => d.x)
+      .attr("cy", d => d.y);
 
     // Reposition links
     this.svg.selectAll(".link")
@@ -254,16 +294,6 @@ class AwsInstanceChart {
       .attr("y1", d => d.source.y)
       .attr("x2", d => d.target.x)
       .attr("y2", d => d.target.y);
-
-    // Reposition nodes
-    this.svg.selectAll(".node circle")
-      .attr("cx", d => d.x)
-      .attr("cy", d => d.y);
-
-    // Reposition labels
-    this.svg.selectAll(".node text")
-      .attr("x", d => d.x + this.getNodeRadius(d) + 4)
-      .attr("y", d => d.y);
   }
 
   /**
@@ -274,7 +304,7 @@ class AwsInstanceChart {
     new AWS.EC2().describeInstances({}, (err, resp) => {
       if(err) return;
 
-      // Build a map of roles -> instances
+      // Go through each instance
       for(var reservation of resp.Reservations) {
         for(var instance of reservation.Instances) {
           // Build a hash of tags (instead of ec2's array format)
@@ -283,10 +313,12 @@ class AwsInstanceChart {
             tags[tag.Key] = tag.Value;
           }
 
-          // Add instance to nodes list
+          // Add instance to internal list
           this.instances.push({
             id: instance.InstanceId,
-            type: instance.InstanceType,
+            nodeType: "instance",
+            instanceId: instance.InstanceId,
+            instanceType: instance.InstanceType,
             tags: tags
           });
         }
