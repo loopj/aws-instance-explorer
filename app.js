40dd21730d484c360ec3b77ef921f2ee2db2ae57
James Smith <james@loopj.com>
2016-05-29 18:33:03 -0700

    Initial work towards configurable filtering and grouping

--- app.js
+++ app.js
@@ -4,13 +4,20 @@ class AwsInstanceChart {
    * @param el the parent element to populate the chart into
    */
   constructor(el) {
+    // Instance node data
+    this.nodes = [];
+
+    // Root node
+    this.rootNode = {name: "root", group: true};
+    this.nodes.push(this.rootNode);
+
     // Configure AWS SDK
     AWS.config.region = 'us-east-1';
 
     // Create a D3 force-directed graph layout
     this.force = d3.layout.force()
       .charge(-200)
-      .on("tick", () => { this.tick() });
+      .on("tick", () => this.tick());
 
     // Create the SVG drawing canvas
     this.svg = d3.select(el).append("svg");
@@ -20,9 +27,26 @@ class AwsInstanceChart {
 
     // Set size and allow window resizing
     this.resize(el);
-    d3.select(window).on("resize", () => {
-      this.resize(el);
-    });
+    d3.select(window).on("resize", () => this.resize(el));
+
+    // Instance filtering rules, for example:
+    //    filterRules.equals.push(["tags.Role", "db"])
+    //    filterRules.equals.push(["type", "r3.2xlarge"])
+    //    filterRules.contains.push(["type", "[^x]large"])
+    //    filterRules.set.push("tags.James")
+    this.filters = {
+      equals: [],
+      notEquals: [],
+      contains: [],
+      notContains: [],
+      set: [],
+      notSet: []
+    };
+
+    // TODO: Instance grouping rules, for example:
+    //    groupingRules.push("tags.Role")
+    //    groupingRules.push("type")
+    this.groups = [];
   }
 
   /**
@@ -57,7 +81,7 @@ class AwsInstanceChart {
    * @param d the d3 datum
    */
   getNodeFill(d) {
-    return d.children ? "#ffffff" : this.fill(d.role);
+    return d.group ? "#ffffff" : this.fill(d.tags.Role);
   }
 
   /**
@@ -65,7 +89,7 @@ class AwsInstanceChart {
    * @param d the d3 datum
    */
   getNodeStroke(d) {
-    return d3.rgb(this.fill(d.role)).darker(2);
+    return d.group ? "#555555" : d3.rgb(this.fill(d.tags.Role)).darker(0.5);
   }
 
   /**
@@ -78,51 +102,141 @@ class AwsInstanceChart {
       "2xlarge", "4xlarge", "8xlarge", "16xlarge", "32xlarge"
     ];
 
-    return d.type ? Math.pow(instanceTypes.indexOf(d.type.split(".")[1]), 1.4) : 1;
+    return d.group ? 3 : Math.pow(instanceTypes.indexOf(d.type.split(".")[1]), 1.4);
   }
 
   /**
-   * Update the node data used to generate the chart
-   * @param nodes the chart node data
+   * Find a key in a (potentially) nested object
+   * @example
+   * // returns "db"
+   * findKey({tags: {Role: "db"}, type: "r3.large"}, "tags.Role")
+   * // returns "r3.large"
+   * findKey({tags: {Role: "db"}, type: "r3.large"}, "type")
    */
-  update(nodes = []) {
-    var links = d3.layout.tree().links(nodes);
+  findKey(key, obj) {
+    try {
+      return key.split(".").reduce((o, i) => o[i], obj);
+    } catch (e) {
+      return undefined;
+    }
+  }
 
-    // Update nodes and links
-    this.force
-      .nodes(nodes)
-      .links(links)
-      .start();
+  /**
+   * Given a set of filter rules, check if they all match using the given test function
+   */
+  matchesFilter(rules, obj, testFunc) {
+    for(var i = 0; i < rules.length; i++) {
+      var [key, expected] = rules[i];
+      var actual = this.findKey(key, obj);
 
-    // Draw nodes
-    var node = this.svg.selectAll(".node")
-      .data(nodes)
+      if(!testFunc(actual, expected)) return false;
+    }
+
+    return true;
+  }
+
+  /**
+   * Check if the given node matches all active filters
+   */
+  matchesAllFilters(node) {
+    // Never filter the root node
+    if(node == this.rootNode) return true;
+
+    // Apply "equals" rules
+    if(!this.matchesFilter(this.filters.equals, node, (a, b) => a == b)) {
+      return false;
+    }
+
+    // Apply "notEquals" rules
+    if(!this.matchesFilter(this.filters.notEquals, node, (a, b) => a != b)) {
+      return false;
+    }
+
+    // Apply "contains" rules
+    if(!this.matchesFilter(this.filters.contains, node, (a, b) => !!a.match(new RegExp(b)))) {
+      return false;
+    }
+
+    // Apply "notContains" rules
+    if(!this.matchesFilter(this.filters.notContains, node, (a, b) => !a.match(new RegExp(b)))) {
+      return false;
+    }
+
+    // Apply "set" rules
+    for(var i = 0; i < this.filters.set.length; i++) {
+      if(this.findKey(this.filters.set[i], node) === undefined) return false;
+    }
+
+    // Apply "notSet" rules
+    for(var i = 0; i < this.filters.notSet.length; i++) {
+      if(this.findKey(this.filters.notSet[i], node) !== undefined) return false;
+    }
+
+    return true;
+  }
+
+  getNodes() {
+    return this.nodes.filter(d => this.matchesAllFilters(d));
+  }
+
+  getLinks(nodes) {
+    return nodes.map(d => ({source: this.rootNode, target: d}));
+  }
+
+  drawNodes(nodes) {
+    var nodeSelection = this.svg.selectAll(".node").data(nodes, d => d.instanceId);
+
+    // Create svg group element for each instance
+    var nodeGroup = nodeSelection
       .enter().append("g")
         .attr("class", "node")
         .call(this.force.drag);
 
-    // Draw circles
-    var circle = node.append("circle")
-      .attr("r", (d) => { return this.getNodeRadius(d); } )
-      .style("fill", (d) => { return this.getNodeFill(d); })
-      .style("stroke", (d) => { return this.getNodeStroke(d); });
+    // Create circle for each AWS instance - add to svg group
+    nodeGroup.append("circle")
+      .attr("r", d => this.getNodeRadius(d))
+      .style("fill", d => this.getNodeFill(d))
+      .style("stroke", d => this.getNodeStroke(d));
 
-    // Draw labels
-    var label = node.append("text")
+    // Create label for each AWS instance - add to svg group
+    nodeGroup.append("text")
       .attr("dy", ".35em")
-      .text(function(d) { return d.name; });
+      .text(d => d.tags ? d.tags.Name : d.name);
 
-    // Draw links
-    var link = this.svg.selectAll(".link")
-      .data(links)
+    // Remove orphaned dom elements
+    nodeSelection.exit().remove();
+  }
+
+  drawLinks(links) {
+    var linkSelection = this.svg.selectAll(".link").data(links, (d) => {
+      return `${d.source.instanceId}-${d.target.instanceId}`;
+    });
+
+    // Create lines for each link
+    linkSelection
       .enter().insert("line", ".node")
         .attr("class", "link");
 
-    // Fade in SVG canvas
-    this.svg.style("opacity", 1e-6)
-      .transition()
-        .duration(5000)
-        .style("opacity", 1);
+    // Remove orphaned dom elements
+    linkSelection.exit().remove();
+  }
+
+  /**
+   * Update the node data used to generate the chart
+   */
+  update() {
+    var nodes = this.getNodes();
+    var links = this.getLinks(nodes);
+
+    // Draw nodes and links
+    this.drawNodes(nodes);
+    this.drawLinks(links);
+
+    // Update force layout
+    this.force
+      .nodes(nodes)
+      .links(links)
+      .start();
   }
 
   /**
@@ -148,15 +262,14 @@ class AwsInstanceChart {
   }
 
   /**
-   * Fetch a map of instances, grouped by role
+   * Load instance data into the chart
    */
-  getInstanceMap(cb) {
+  loadData() {
     // API request to list all EC2 instances
-    var request = new AWS.EC2().describeInstances({}, (err, resp) => {
-      if(err) return cb(err);
+    new AWS.EC2().describeInstances({}, (err, resp) => {
+      if(err) return;
 
       // Build a map of roles -> instances
-      var roles = {};
       for(var reservation of resp.Reservations) {
         for(var instance of reservation.Instances) {
           // Build a hash of tags (instead of ec2's array format)
@@ -165,47 +278,17 @@ class AwsInstanceChart {
             tags[tag.Key] = tag.Value;
           }
 
-          // Add instance to role, create role if not seen before
-          roles[tags["Role"]] = roles[tags["Role"]] || [];
-          roles[tags["Role"]].push({
+          // Add instance to nodes list
+          this.nodes.push({
             instanceId: instance.InstanceId,
             type: instance.InstanceType,
-            name: tags["Name"],
-            role: tags["Role"]
+            tags: tags
           });
         }
       }
 
-      // Return instance map
-      cb(null, roles);
-    });
-  }
-
-  /**
-   * Load instance data into the chart
-   */
-  loadData() {
-    this.getInstanceMap((err, instanceMap) => {
-      // Build a flattened tree of nodes from the hash, for d3
-      var nodes = [];
-      var rootNode = {name: "root", children: []};
-      nodes.push(rootNode);
-
-      // Add each role to the tree
-      for(var role in instanceMap) {
-        var roleNode = {name: role, children: []};
-        nodes.push(roleNode);
-        rootNode.children.push(roleNode);
-
-        // Add each instance to the tree
-        instanceMap[role].forEach((instanceNode) => {
-          nodes.push(instanceNode);
-          roleNode.children.push(instanceNode);
-        });
-      }
-
       // Update the chart with the new data
-      this.update(nodes);
+      this.update();
     });
   }
 }
